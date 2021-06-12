8189f7a98dc1db2605c3ec522df11167e1142cbf
Olivier9493 <oli4@pachyderme.li>
2021-06-12 18:10:40 +0200

    Update holidays Liechternstein

--- holidays/countries/liechtenstein.py
+++ holidays/countries/liechtenstein.py
@@ -11,7 +11,6 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-import warnings
 from datetime import date
 
 from dateutil.easter import easter
@@ -21,7 +20,16 @@ from holidays.constants import JAN, FEB, MAR, MAY, AUG, SEP, NOV, DEC
 from holidays.holiday_base import HolidayBase
 
 class Liechtenstein(HolidayBase):
-    # https://publicholidays.li/de/
+    # https://en.wikipedia.org/wiki/Public_holidays_in_Liechtenstein
+    """
+    This class returns the holidays for Liechtenstein (LI/LIE/FL)
+    Even if the country is pretty small, the following date aren't offical holidays
+    but only for some branches (Bankfeiertag) and mainly not worked
+   - "Berchtoldstag" : Jan 2nd (Saint Berchtold's Day)
+   - "Fasnachtsdienstag" : 47 days before Easter (Carnival - Shrove Tuesday)
+   - "Karfreitag" : Friday before Easter
+   - "Heiliger Abend"  : Dec 24th (Christmas Eve)
+    """
 
     def __init__(self, **kwargs):
         self.country = "LI"
@@ -29,9 +37,11 @@ class Liechtenstein(HolidayBase):
 
     def _populate(self, year):
         self[date(year, JAN, 1)] = "Neujahr"
+        self[date(year, JAN, 2)] = "Berchtoldstag"
         self[date(year, JAN, 6)] = "Heilige Drei König"
         self[date(year, FEB, 2)] = "Lichtmess"
         self[date(year, MAR, 19)] = "Joseftag"
+        self[easter(year) - rd(days=-47)] = "Fasnachtsdienstag"
         self[easter(year) - rd(days=2)] = "Karfreitag"
         self[easter(year)] = "Ostersonntag"
         self[easter(year) + rd(days=1)] = "Ostermontag"
@@ -44,6 +54,7 @@ class Liechtenstein(HolidayBase):
         self[date(year, SEP, 8)] = "Mariä Geburt"
         self[date(year, NOV, 1)] = "Allerheiligen"
         self[date(year, DEC, 8)] = "Mariä Empfängnis"
+        self[date(year, DEC, 24)] = "Heiliger Abend"
         self[date(year, DEC, 25)] = "Weihnachten"
         self[date(year, DEC, 26)] = "Stephanstag"
 
